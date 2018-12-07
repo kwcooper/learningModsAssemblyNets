3d97cae9788eac935cc43e6b55b8b8d2b3a86f1c
Keiland <officialkwade@gmail.com>
2018-12-07 03:38:04 -0500

    Ud, big overhaul

--- networkMeasures.m
+++ networkMeasures.m
@@ -2,7 +2,9 @@
 
 % Define inter-rat struct
 btRats.degs = zeros(4,3);
-plt = 1;
+btRats.deg = {};
+btRats.meanq = zeros(4,3);
+plt = 0;
 
 tic
 % for each rat, run analysis
@@ -36,7 +38,7 @@ for rat = 1:4
     % For each phase, run the network analysis
     netStats = struct;
     fields = {'pre','maz','pst'};
-    for i = 1:3
+    for i = 1:3% size(fields,2)
         disp(strjoin(["Phase:", fields{i}]))
         %keyboard; 
         
@@ -135,7 +137,12 @@ for rat = 1:4
         %netStats.(fields{i}).          =    ;
         
         % Between rat
-        btRats.degs(rat,i) = mean(deg);
+        btRats.degs(rat,i)       = mean(deg);
+        btRats.meanq(rat,i)      = mean(q);
+        btRats.phase(i).deg{rat} = sort(deg, 'descend');
+        btRats.phase(i).CIJ{rat} = CIJ;
+        btRats.phase(i).ci{rat}  = mean(max(ci));
+        btRats.phase(i).q{rat}   = mean(q);
         disp(' ')
     end
     
@@ -150,17 +157,131 @@ disp(datestr(datenum(0,0,0,0,0,t),'HH:MM:SS'))
 
 %% Between rats...
 
+if 0 % plot connectivity matricies for one rat
+    h = figure('Position', [700, 100, 700, 225]); hold on;
+    subplot(1,3,1); 
+    imagesc(btRats.phase(1).CIJ{1})
+    title('PRE'); 
+    ax = gca; 
+    set(gca,'YTickLabel',[]); set(gca,'XTickLabel',[]); 
+    ax.FontSize = 14;
+
+    subplot(1,3,2); 
+    imagesc(btRats.phase(2).CIJ{1})
+    title({'Connectivity Matricies'; 'Maze'})
+    ax = gca; 
+    set(gca,'YTickLabel',[]); set(gca,'XTickLabel',[]); 
+    ax.FontSize = 14;
+
+    subplot(1,3,3); 
+    imagesc(btRats.phase(3).CIJ{1})
+    title('POST');
+    ax = gca;
+    set(gca,'YTickLabel',[]); set(gca,'XTickLabel',[]); 
+    ax.FontSize = 14;
+
+    
+    figName = "Figs/CIJ/achCIJ";
+    savefig(h, char(figName+".fig"));
+    saveas(h,  char(figName+".png"));
+end
+
+
 if 0 % Average degree increases?
-    figure; hold on;
+    h = figure; hold on;
+    cVec = [[0 1 0]; [1 0 1]; [0 1 1]; [1 0 0]];
     for zz = 1:size(btRats.degs,1)
-        %scatter(1:3,tst(zz,:),50,'filled');
-        plot(btRats.degs(zz,:)');
+        hold on;
+        scatter(1:3,btRats.degs(zz,:),70,cVec(zz,:),'filled');
+        line(1:3,btRats.degs(zz,:),'Color',cVec(zz,:))
+        %plot(btRats.degs(zz,:)', 'LineWidth',3);
+        
+        ylabel('Average Degree');
+        set(gca,'XTick',[0 1 2 3 4])
+        names = {'PRE';'MAZE';'POST'};
+        set(gca,'xtick',[1:3],'xticklabel',names)
+        xlim([.5 3.5]);
+        ax = gca; ax.FontSize = 14; 
     end
+    
+    figName = "Figs/degDist/avgDeg";
+    savefig(h, char(figName+".fig"));
+    saveas(h,  char(figName+".png"));
+end
+
+
+if 0 % Average q?
+    h = figure; hold on;
+    cVec = [[0 1 0]; [1 0 1]; [0 1 1]; [1 0 0]];
+    for zz = 1:size(btRats.meanq,1)
+        hold on;
+        scatter(1:3,btRats.meanq(zz,:),70,cVec(zz,:),'filled');
+        line(1:3,btRats.meanq(zz,:),'Color',cVec(zz,:))
+        %plot(btRats.degs(zz,:)', 'LineWidth',3);
+        
+        ylabel('Average Q Value');
+        set(gca,'XTick',[0 1 2 3 4])
+        names = {'PRE';'MAZE';'POST'};
+        set(gca,'xtick',[1:3],'xticklabel',names)
+        xlim([.5 3.5]);
+        ax = gca; ax.FontSize = 14; 
+    end
+    
+    figName = "Figs/avgQ";
+    savefig(h, char(figName+".fig"));
+    saveas(h,  char(figName+".png"));
 end
 
 
 
 
+if 0 % plot degree distribution
+        cVec = [[0 1 0]; [1 0 1]; [0 1 1]; [1 0 0]];
+
+h = figure; 
+subplot(3,1,1); hold on; 
+cellfun(@plot, btRats.phase(1).deg); title({' Degree Distribution'; 'PRE'})
+ylabel('Degree');
+ax = gca; ax.FontSize = 14;
+
+subplot(3,1,2); hold on; 
+cellfun(@plot,btRats.phase(2).deg); title('MAZE')
+ylabel('Degree');
+ax = gca; ax.FontSize = 14;
+
+subplot(3,1,3); hold on; 
+cellfun(@plot,btRats.phase(3).deg); title('POST')
+ylabel('Degree'); xlabel('Node');
+ax = gca; ax.FontSize = 14;
+
+figName = "Figs/degDist/degs";
+savefig(h, char(figName+".fig"));
+saveas(h,  char(figName+".png"));
+
+end
+% 1. e? 2. 
+
+%%
+if 0 
+N=length(A{1});
+T=length(A);
+B=spalloc(N*T,N*T,N*N*T+2*N*T);
+twomu=0;
+for s=1:T
+    k=sum(A{s});
+    twom=sum(k);
+    twomu=twomu+twom;
+    indx=[1:N]+(s-1)*N;
+    B(indx,indx)=A{s}-gamma*k'*k/twom;
+end
+twomu=twomu+2*omega*N*(T-1);
+B = B + omega*spdiags(ones(N*T,2),[-N,N],N*T,N*T);
+[S,Q] = genlouvain(B);
+Q = Q/twomu
+S = reshape(S,N,T);
+
+end
+% small omega .01
 
 %% Visuals testing
 if 0
